07d78969242bd4119ca824e8c418f3450ce48291
Deborah <liz.joy43@gmail.com>
2024-09-13 14:17:23 -0600

    Improved the user experience

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class ="blink">⏳Generating a poem for you about ${instructionsInput.value}</div>`;
+
   console.log("generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
