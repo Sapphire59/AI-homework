bac3d157f5f39888fdbc185776e9f8b0ab0e0bc4
Deborah <liz.joy43@gmail.com>
2024-09-12 22:42:01 -0600

    basic JS form, added typewriter

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: [
+      "The Cardinal has come again, he all the brake salutes, his music floods the silent glen",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form-generator");
+poemFormElement.addEventListener("submit", generatePoem);
